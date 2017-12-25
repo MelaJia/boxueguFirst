50f32a06ef7a04829bb34acbf2020516fd67b6b4
MelaJia <838672863@qq.com>
2017-12-25 17:06:04 +0800

    实现｀登录｀页面UI

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/RegisterActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/RegisterActivity.java
@@ -1,10 +1,10 @@
-package cn.edu.gdmec.android.boxuegu;
+package cn.edu.gdmec.android.boxuegu.activity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +13,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import cn.edu.gdmec.android.boxuegu.R;
 import cn.edu.gdmec.android.boxuegu.utils.Md5Utils;
 
 public class RegisterActivity extends AppCompatActivity {
