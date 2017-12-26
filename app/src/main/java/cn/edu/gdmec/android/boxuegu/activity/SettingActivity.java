b73a67ad3515a4684ba775f8933873c8886f29cb
MelaJia <8386872863@qq.com>
2017-12-26 11:43:40 +0800

    我－>设置界面->退出登录逻辑代码

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/SettingActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/SettingActivity.java
@@ -1,7 +1,16 @@
 package cn.edu.gdmec.android.boxuegu.activity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.concurrent.locks.ReentrantLock;
 
 import cn.edu.gdmec.android.boxuegu.R;
 
@@ -11,5 +20,60 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        init();
+    }
+
+    private void init() {
+        TextView tv_main_title = (TextView) findViewById(R.id.tv_main_title);
+        tv_main_title.setText("设置");
+        TextView tv_back = (TextView) findViewById(R.id.tv_back);
+        RelativeLayout rl_title_bar = (RelativeLayout) findViewById(R.id.title_bar);
+        rl_title_bar.setBackgroundColor(Color.parseColor("#3084FF"));
+
+        RelativeLayout rl_modify_psw = (RelativeLayout) findViewById(R.id.rl_modify_psw);
+        RelativeLayout rl_security_setting = (RelativeLayout) findViewById(R.id.rl_security_setting);
+
+        RelativeLayout rl_exit_login = (RelativeLayout) findViewById(R.id.rl_exit_login);
+        tv_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SettingActivity.this.finish();
+            }
+        });
+        rl_modify_psw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        rl_security_setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //跳转到设置密保界面
+
+            }
+        });
+        rl_exit_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SettingActivity.this,"退出登录成功",Toast.LENGTH_SHORT).show();
+                clearLoginStatus();
+                //把退出登录成功后的状态传递到mainactivity中
+                Intent data = new Intent();
+                data.putExtra("isLogin",false);
+                setResult(RESULT_OK,data);
+                SettingActivity.this.finish();
+
+            }
+        });
+
+    }
+    private void clearLoginStatus(){
+        SharedPreferences sp = getSharedPreferences("loginInfo",MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putBoolean("isLogin",false);
+        editor.putString("loginUserName","");
+        editor.commit();
     }
 }
